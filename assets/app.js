76299ad3bcd2cf8c1393848a3eb599b2accc8dd6
ChrisStavnes <ChrisStavnes@gmail.com>
2018-03-19 20:35:30 -0500

    format changes

--- assets/app.js
+++ assets/app.js
@@ -3,15 +3,15 @@
 
 //Timer for quiz
 
-let count=60;
+let count = 60;
 
-let secTimer= setInterval(timer, 1000);
+let secTimer = setInterval(timer, 1000);
 
 function timer() {
-    count = count-1;
+    count = count - 1;
     console.log(count)
     if (count <= 0) {
-        
+
         clearInterval(secTimer);
         console.log(count)
         //counter ended do stuff here
@@ -29,12 +29,12 @@ function timer() {
 //      $("#time-left").append("15 Seconds Remaining");
 //      console.log(correctCounter)
 //  }
- 
+
 
 //  function timeUp() {
 //     $("#time-left").empty();
 //     $("#time-left").append("Times Up");
-    
+
 //  }
 
 
@@ -42,66 +42,66 @@ let correctCounter = 0;
 let incorrectCounter = 0;
 
 //once submit button is clicked checks all questions and displayes correct and incorrect answers
-$( "#button" ).on( "click", function() {
+$("#button").on("click", function () {
 
     let answer1 = $("input[name='coaches']:checked").val();
     if (answer1 === "Sir Alex Ferguson") {
-        
+
         correctCounter++;
-        
+
     } else {
 
         incorrectCounter++;
     }
 
     let answer2 = $("input[name='trophies']:checked").val();
-    
+
     if (answer2 === "20") {
-       
+
         correctCounter++;
 
     } else {
-       
+
         incorrectCounter++;
     }
 
     let answer3 = $("input[name='player']:checked").val();
 
     if (answer3 === "Paul") {
-       
+
         correctCounter++;
 
     } else {
-        
+
         incorrectCounter++;
     }
-    
+
     let answer4 = $("input[name='goals']:checked").val();
 
     if (answer4 === "Wayne Rooney") {
-       
+
         correctCounter++;
 
     } else {
-        
+
         incorrectCounter++;
     }
 
     let answer5 = $("input[name='notplayer']:checked").val();
 
     if (answer5 === "Lionel Messi") {
-       
+
         correctCounter++;
 
     } else {
-        
+
         incorrectCounter++;
     }
-    
+
     //dislaying scores and displaying aswers
     $(".correct").append(correctCounter)
     $(".incorrect").append(incorrectCounter)
-    
+
     $(".first").append('<p>Correct Answer: Sir Alex Ferguson</p>')
     $(".second").append('<p>Correct Answer: 20</p>')
     $(".third").append('<p>Correct Answer: Paul Pogba</p>')
@@ -109,7 +109,7 @@ $( "#button" ).on( "click", function() {
     $(".fifth").append('<p>Correct Answer: Lionel Messi</p>')
 
 
-    });
- 
+});
+
 
 
